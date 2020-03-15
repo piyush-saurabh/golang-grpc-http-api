2913ac85a204483cba4a660c8a5bb334ab9ac0ad
Piyush Saurabh <saurabh.piyush@rediffmail.com>
2020-03-15 16:12:23 +0530

    updated server main

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.3.5
 	google.golang.org/grpc v1.28.0
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0
 )
--- pkg/cmd/server/main.go
+++ pkg/cmd/server/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/piyush-saurabh/golang-grpc-http-api/pkg/cmd"
 )
 
 func main() {
-	if err := cmd.RunServer(); err != nil {
+	if err := RunServer(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
